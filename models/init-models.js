898450ad55c1fa682944854fa1ce4b65ac6e0ce6
Alastear <miraistorm@gmail.com>
2022-10-11 20:02:07 +0700

    f

--- models/init-models.js
+++ models/init-models.js
@@ -1,68 +1,68 @@
 var DataTypes = require("sequelize").DataTypes;
-var _dega_achievement = require("./dega_achievement");
-var _dega_events = require("./dega_events");
-var _dega_gacha_log = require("./dega_gacha_log");
-var _dega_monster_data = require("./dega_monster_data");
-var _dega_monster_inv = require("./dega_monster_inv");
-var _dega_monster_team = require("./dega_monster_team");
-var _dega_news = require("./dega_news");
-var _dega_pvp_log = require("./dega_pvp_log");
-var _dega_quest = require("./dega_quest");
+var _Dega_Achievement = require("./Dega_Achievement");
+var _Dega_Events = require("./Dega_Events");
+var _Dega_Gacha_log = require("./Dega_Gacha_log");
+var _Dega_Monster_Data = require("./Dega_Monster_Data");
+var _Dega_Monster_Inv = require("./Dega_Monster_Inv");
+var _Dega_Monster_Team = require("./Dega_Monster_Team");
+var _Dega_News = require("./Dega_News");
+var _Dega_PVP_log = require("./Dega_PVP_log");
+var _Dega_Quest = require("./Dega_Quest");
+var _Monster_Active_Skill = require("./Monster_Active_Skill");
+var _Monster_Status_Effect = require("./Monster_Status_Effect");
+var _User_Login_Log = require("./User_Login_Log");
+var _Users = require("./Users");
 var _factory = require("./factory");
 var _factory_type = require("./factory_type");
 var _island = require("./island");
 var _land = require("./land");
-var _monster_active_skill = require("./monster_active_skill");
-var _monster_status_effect = require("./monster_status_effect");
-var _user_login_Log = require("./user_login_Log");
-var _users = require("./users");
 
 function initModels(sequelize) {
-  var dega_achievement = _dega_achievement(sequelize, DataTypes);
-  var dega_events = _dega_events(sequelize, DataTypes);
-  var dega_gacha_log = _dega_gacha_log(sequelize, DataTypes);
-  var dega_monster_data = _dega_monster_data(sequelize, DataTypes);
-  var dega_monster_inv = _dega_monster_inv(sequelize, DataTypes);
-  var dega_monster_team = _dega_monster_team(sequelize, DataTypes);
-  var dega_news = _dega_news(sequelize, DataTypes);
-  var dega_pvp_log = _dega_pvp_log(sequelize, DataTypes);
-  var dega_quest = _dega_quest(sequelize, DataTypes);
+  var Dega_Achievement = _Dega_Achievement(sequelize, DataTypes);
+  var Dega_Events = _Dega_Events(sequelize, DataTypes);
+  var Dega_Gacha_log = _Dega_Gacha_log(sequelize, DataTypes);
+  var Dega_Monster_Data = _Dega_Monster_Data(sequelize, DataTypes);
+  var Dega_Monster_Inv = _Dega_Monster_Inv(sequelize, DataTypes);
+  var Dega_Monster_Team = _Dega_Monster_Team(sequelize, DataTypes);
+  var Dega_News = _Dega_News(sequelize, DataTypes);
+  var Dega_PVP_log = _Dega_PVP_log(sequelize, DataTypes);
+  var Dega_Quest = _Dega_Quest(sequelize, DataTypes);
+  var Monster_Active_Skill = _Monster_Active_Skill(sequelize, DataTypes);
+  var Monster_Status_Effect = _Monster_Status_Effect(sequelize, DataTypes);
+  var User_Login_Log = _User_Login_Log(sequelize, DataTypes);
+  var Users = _Users(sequelize, DataTypes);
   var factory = _factory(sequelize, DataTypes);
   var factory_type = _factory_type(sequelize, DataTypes);
   var island = _island(sequelize, DataTypes);
   var land = _land(sequelize, DataTypes);
-  var monster_active_skill = _monster_active_skill(sequelize, DataTypes);
-  var monster_status_effect = _monster_status_effect(sequelize, DataTypes);
-  var user_login_Log = _user_login_Log(sequelize, DataTypes);
-  var users = _users(sequelize, DataTypes);
 
-  dega_achievement.belongsTo(users, { as: "user", foreignKey: "user_id"});
-  users.hasMany(dega_achievement, { as: "dega_achievements", foreignKey: "user_id"});
-  dega_monster_inv.belongsTo(users, { as: "user", foreignKey: "user_id"});
-  users.hasMany(dega_monster_inv, { as: "dega_monster_invs", foreignKey: "user_id"});
-  dega_monster_team.belongsTo(users, { as: "user", foreignKey: "user_id"});
-  users.hasMany(dega_monster_team, { as: "dega_monster_teams", foreignKey: "user_id"});
-  user_login_Log.belongsTo(users, { as: "user", foreignKey: "user_id"});
-  users.hasMany(user_login_Log, { as: "user_login_Logs", foreignKey: "user_id"});
+  Dega_Achievement.belongsTo(Users, { as: "user", foreignKey: "user_id"});
+  Users.hasMany(Dega_Achievement, { as: "Dega_Achievements", foreignKey: "user_id"});
+  Dega_Monster_Inv.belongsTo(Users, { as: "user", foreignKey: "user_id"});
+  Users.hasMany(Dega_Monster_Inv, { as: "Dega_Monster_Invs", foreignKey: "user_id"});
+  Dega_Monster_Team.belongsTo(Users, { as: "user", foreignKey: "user_id"});
+  Users.hasMany(Dega_Monster_Team, { as: "Dega_Monster_Teams", foreignKey: "user_id"});
+  User_Login_Log.belongsTo(Users, { as: "user", foreignKey: "user_id"});
+  Users.hasMany(User_Login_Log, { as: "User_Login_Logs", foreignKey: "user_id"});
 
   return {
-    dega_achievement,
-    dega_events,
-    dega_gacha_log,
-    dega_monster_data,
-    dega_monster_inv,
-    dega_monster_team,
-    dega_news,
-    dega_pvp_log,
-    dega_quest,
+    Dega_Achievement,
+    Dega_Events,
+    Dega_Gacha_log,
+    Dega_Monster_Data,
+    Dega_Monster_Inv,
+    Dega_Monster_Team,
+    Dega_News,
+    Dega_PVP_log,
+    Dega_Quest,
+    Monster_Active_Skill,
+    Monster_Status_Effect,
+    User_Login_Log,
+    Users,
     factory,
     factory_type,
     island,
     land,
-    monster_active_skill,
-    monster_status_effect,
-    user_login_Log,
-    users,
   };
 }
 module.exports = initModels;
